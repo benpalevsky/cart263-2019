aa21503146806519bc59705ec1c6a353305ddd79
Ben Palevsky <bpalevsky@gmail.com>
2019-01-24 12:46:23 -0800

    a2: food now has velocity properties which are set randomly based on maximum speed

--- assignments/assignment2/js/Food.js
+++ assignments/assignment2/js/Food.js
@@ -10,11 +10,13 @@ class Food extends Agent {
   // Pass arguments on to the super() constructor (e.g. for Agent)
   // Also set a minimum and maximum size for this food object which it
   // will vary between when it resets
-  constructor(x,y,minSize,maxSize, maxSpeed) {
+  constructor(x,y,vx,vy,minSize,maxSize,maxSpeed) {
     super(x,y,random(minSize,maxSize),'#55cccc');
     this.minSize = minSize;
     this.maxSize = maxSize;
     this.maxSpeed = maxSpeed;
+    this.vx = random(-maxSpeed,maxSpeed);
+    this.vy = random(-maxSpeed,maxSpeed);
   }
 
   // reset()
