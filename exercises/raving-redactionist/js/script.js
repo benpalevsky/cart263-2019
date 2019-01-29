60fe3efcb71486ac13586f7144191215b0190599
Ben Palevsky <bpalevsky@gmail.com>
2019-01-29 12:01:33 -0800

    raving-reactionist: half done, shitty commit

---
+++ exercises/raving-redactionist/js/script.js
@@ -0,0 +1,15 @@
+/*****************
+
+Title of Project
+Author Name
+
+This is a template. You must fill in the title,
+author, and this description to match your project!
+
+******************/
+
+$(document).ready(setInterval(update, 500));
+
+function update() {
+  console.log("Update!");
+}
