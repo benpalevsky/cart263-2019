036ac0934ef99c81266ee936df100ff75d821e75
Ben Palevsky <bpalevsky@gmail.com>
2019-01-29 14:58:31 -0800

    a3: modified the logic so that if statements now check for spans and classes within spans

--- assignments/assignment3/js/script.js
+++ assignments/assignment3/js/script.js
@@ -18,23 +18,25 @@ function setup() {
 }
 
 function update() {
-  console.log("Update!");
   $spans.each(updateSpans);
   $spans.on("click", spanClicked);
 }
 
 function updateSpans() {
-  console.log("Updating spans!");
   var r = Math.random();
 
   if (r < 0.1) {
-    $(this).removeClass("redacted");
-    $(this).addClass("revealed");
+    if ($(this).hasClass("redacted")) {
+      $(this).removeClass("redacted");
+      $(this).addClass("revealed");
+    }
   }
 
 }
 
 function spanClicked() {
-  $(this).removeClass("revealed");
-  $(this).addClass("redacted");
+  if ($(this).hasClass("revealed")) {
+    $(this).removeClass("revealed");
+    $(this).addClass("redacted");
+  }
 }
