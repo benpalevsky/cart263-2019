09c0d77c1f92abc842fda8650f8b82c720f38422
Ben Palevsky <bpalevsky@gmail.com>
2019-02-06 13:00:24 -0500

    jqui: fly buzz sound now loops and crunch plays

--- exercises/simple-jquery-ui/js/script.js
+++ exercises/simple-jquery-ui/js/script.js
@@ -11,25 +11,46 @@ author, and this description to match your project!
 var $mouth;
 var $fly;
 
+var $buzzSound
+var $crunchSound
+
 $(document).ready(setup);
 
 function setup() {
+  $buzzSound = $("#buzz");
+  $crunchSound = $("#crunch");
+
+
+  $fly = $("#fly");
+  $fly.draggable();
+  $fly.mousedown(function() {
+    $buzzSound[0].play();
+  });
+
+  $fly.mouseup(function() {
+    $buzzSound[0].pause();
+  });
+
   $mouth = $("#mouth");
   $mouth.droppable({
     drop: function(event, ui) {
       ui.draggable.remove();
       setInterval(chew, 500);
+      $crunchSound[0].play();
+
     }
   });
-  $fly = $("#fly");
-  $fly.draggable();
 }
 
 function chew() {
 
   if ($mouth.attr("src") == "assets/images/mouth-open.png") {
     $mouth.attr("src", "assets/images/mouth-closed.png");
+    $crunchSound[0].play();
+
   } else if ($mouth.attr("src") == "assets/images/mouth-closed.png") {
     $mouth.attr("src", "assets/images/mouth-open.png");
+    $crunchSound[0].play();
+
   }
 }
