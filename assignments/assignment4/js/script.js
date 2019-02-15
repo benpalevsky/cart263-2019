87ceec14d3e0b9e6888cdaa795626e8398757809
Ben Palevsky <bpalevsky@gmail.com>
2019-02-15 17:07:25 -0500

    a4: project import

---
+++ assignments/assignment4/js/script.js
@@ -0,0 +1,56 @@
+/*****************
+
+Title of Project
+Author Name
+
+This is a template. You must fill in the title,
+author, and this description to match your project!
+
+******************/
+
+var $mouth;
+var $fly;
+
+var $buzzSound
+var $crunchSound
+
+$(document).ready(setup);
+
+function setup() {
+  $buzzSound = $("#buzz");
+  $crunchSound = $("#crunch");
+
+
+  $fly = $("#fly");
+  $fly.draggable();
+  $fly.mousedown(function() {
+    $buzzSound[0].play();
+  });
+
+  $fly.mouseup(function() {
+    $buzzSound[0].pause();
+  });
+
+  $mouth = $("#mouth");
+  $mouth.droppable({
+    drop: function(event, ui) {
+      ui.draggable.remove();
+      setInterval(chew, 500);
+      $crunchSound[0].play();
+
+    }
+  });
+}
+
+function chew() {
+
+  if ($mouth.attr("src") == "assets/images/mouth-open.png") {
+    $mouth.attr("src", "assets/images/mouth-closed.png");
+    $crunchSound[0].play();
+
+  } else if ($mouth.attr("src") == "assets/images/mouth-closed.png") {
+    $mouth.attr("src", "assets/images/mouth-open.png");
+    $crunchSound[0].play();
+
+  }
+}
