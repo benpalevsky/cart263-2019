619325e3fd51e00d1bfa2508a79d73912fd526cd
Ben Palevsky <bpalevsky@gmail.com>
2019-03-06 21:07:10 -0500

    project2: annyang now listens for a word

--- projects/project2/js/script.js
+++ projects/project2/js/script.js
@@ -1,17 +1,12 @@
-let currentSentence;
-let currentSentenceAuthor;
-let transformedSentence = ""
+let currentQuote;
+let currentAuthor;
+let spokenSentence;
+let transformedSentence;
 let db;
 
 if (annyang) {
   // Let's define our first command. First the text we expect, and then the function it should call
-  var commands = {
-    'show tps report': function() {
-      $('#tpsreport').animate({
-        bottom: '-100px'
-      });
-    }
-  };
+  var commands = {};
 
 
   // Add our commands to annyang
@@ -21,9 +16,10 @@ if (annyang) {
   annyang.start();
 
   annyang.addCallback('result', function(whatWasHeardArray) {
-    currentSentence = whatWasHeardArray;
-    //responsiveVoice.speak("hello");
-    transformedSentence = RiTa.similarBySound(currentSentence[0]);
+    //console.log(whatWasHeardArray);
+    spokenSentence = whatWasHeardArray[0];
+
+    transformedSentence = RiTa.similarBySound(spokenSentence);
 
   });
 }
@@ -37,11 +33,14 @@ function preload() {
 function setup() {
 
   //hard coded 103 because I can't get the length of objects for some reason
-  currentSentence = db[floor(random(0, 103))].quote;
-  currentSentenceAuthor = db[floor(random(0, 103))].name;
+  currentQuote = db[floor(random(0, 103))].quote;
+  currentAuthor = db[floor(random(0, 103))].name;
 
-  $("h1").text(currentSentence);
-  $("p").text(currentSentenceAuthor);
+  $("h1").text(currentQuote).hide()
+    .fadeIn(2000);
+  $("p").text(currentAuthor).hide()
+    .fadeIn(1000);
+
 
 
 }
