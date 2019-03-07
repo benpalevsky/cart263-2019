af3b22cdb40276c0657b9f92bde49a083c6fa4da
Ben Palevsky <bpalevsky@gmail.com>
2019-03-06 20:49:06 -0500

    project2: page now spits out a random quote on refresh

--- projects/project2/js/script.js
+++ projects/project2/js/script.js
@@ -1,32 +1,52 @@
-/*****************
+let currentSentence;
+let currentSentenceAuthor;
+let transformedSentence = ""
+let db;
 
-Project 2
-Ben Palevsky
+if (annyang) {
+  // Let's define our first command. First the text we expect, and then the function it should call
+  var commands = {
+    'show tps report': function() {
+      $('#tpsreport').animate({
+        bottom: '-100px'
+      });
+    }
+  };
 
-******************/
 
+  // Add our commands to annyang
+  annyang.addCommands(commands);
+
+  // Start listening. You can call this here, or attach this call to an event, button, etc.
+  annyang.start();
+
+  annyang.addCallback('result', function(whatWasHeardArray) {
+    currentSentence = whatWasHeardArray;
+    //responsiveVoice.speak("hello");
+    transformedSentence = RiTa.similarBySound(currentSentence[0]);
+
+  });
+}
 
 function preload() {
-
-
+  font = loadFont('assets/fonts/SourceSansPro-Regular.otf');
+  db = loadJSON('js/data/quotes.json');
 }
 
 
 function setup() {
 
+  //hard coded 103 because I can't get the length of objects for some reason
+  currentSentence = db[floor(random(0, 103))].quote;
+  currentSentenceAuthor = db[floor(random(0, 103))].name;
+
+  $("h1").text(currentSentence);
+  $("p").text(currentSentenceAuthor);
+
 
 }
-
 
 function draw() {
-
-
-}
-
-
-
-function mouseClicked() {
-
 
 
 }
