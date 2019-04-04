fd07b7da074c9d2913ab6bae1395d0f6dda51221
Ben Palevsky <bpalevsky@gmail.com>
2019-04-04 17:08:54 -0400

    a8: some ideation

--- assignments/assignment8/js/script.js
+++ assignments/assignment8/js/script.js
@@ -10,23 +10,40 @@
 //   });
 // }
 
+//types of questions
+//1. true false
+//2. multiple choice
+//3. unique answer
+
+//moving bucket and you have to drop letters into it
+//matter.js + p5.js ?
+
 //answer trivia questions with terrible terrible text UI
 
 //ideas for horrible UI
 
 // always have a timer
 // plinko
+// spelling with sliders
+
+//or pivot entirely and just make a draw on faces game.
+//basically we have the draw on faces that was fun
+//and the ui hell idea that is ok
+
+//I think I'm going to make draw on faces??
+
+}
 
 let request;
 let question;
 
 $(document).ready(function() {
-  getQuestion();
+getQuestion();
 });
 
 function getQuestion() {
-  request = $.getJSON('https://opentdb.com/api.php?amount=1&difficulty=easy').done(function() {
-    question = request.responseJSON.results[0].question;
-    $('#question').html(question);
-  });
+request = $.getJSON('https://opentdb.com/api.php?amount=1&difficulty=easy').done(function() {
+  question = request.responseJSON.results[0].question;
+  $('#question').html(question);
+});
 }
