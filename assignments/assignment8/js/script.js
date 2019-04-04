d0d93e09421f887db226c1fe187f7454e246a3b0
Ben Palevsky <bpalevsky@gmail.com>
2019-04-04 16:43:43 -0400

    a8: successfully pulling trivia questions from a URL

--- assignments/assignment8/js/script.js
+++ assignments/assignment8/js/script.js
@@ -1,11 +1,32 @@
-function getImage() {
-  var flickerAPI = "https://api.flickr.com/services/feeds/photos_public.gne?jsoncallback=?";
-  $.getJSON(flickerAPI, {
-    tags: "juventus",
-    tagmode: "any",
-    format: "json"
-  }).done(function(data) {
-    var random = Math.floor(Math.random() * data.items.length);
-    $('img').attr('src', data.items[random].media.m);
+// function getImage() {
+//   var flickerAPI = "https://api.flickr.com/services/feeds/photos_public.gne?jsoncallback=?";
+//   $.getJSON(flickerAPI, {
+//     tags: "juventus",
+//     tagmode: "any",
+//     format: "json"
+//   }).done(function(data) {
+//     var random = Math.floor(Math.random() * data.items.length);
+//     $('img').attr('src', data.items[random].media.m);
+//   });
+// }
+
+//answer trivia questions with terrible terrible text UI
+
+//ideas for horrible UI
+
+// always have a timer
+// plinko
+
+let request;
+let question;
+
+$(document).ready(function() {
+  getQuestion();
+});
+
+function getQuestion() {
+  request = $.getJSON('https://opentdb.com/api.php?amount=1&difficulty=easy').done(function() {
+    question = request.responseJSON.results[0].question;
+    $('#question').html(question);
   });
 }
