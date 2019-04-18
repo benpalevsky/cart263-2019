a8ac98a5fbeeabe083fb39e74c765c8975d860a9
Ben Palevsky <bpalevsky@gmail.com>
2019-04-18 16:16:13 -0400

    a9: trying to make a cannon, big creative push

--- assignments/assignment9/sketch2.js
+++ assignments/assignment9/sketch2.js
@@ -18,7 +18,6 @@ const TYPE = {
 const category = {}
 let barriers = [];
 let correctIndex;
-let cannon;
 //to apply force
 //ball.body.force = {x: 0.1,y: 0.1};
 //let people pick their own trivia?
@@ -26,14 +25,6 @@ let cannon;
 //can even have the same physics game/game for each category so players can get better at iterations
 //lets start
 //answers should just randomly change
-//Simple physics games.
-//Constraints
-//1. Shapes can only be circles and/or blocks
-//2. Games must only have one input (mouse movement and click is considered two)
-//3. Games should be intuitive enough to be understood and played on the first go
-//4. Will figure out how to make these games into trivia later
-//Ideas
-//1. Cannon
 function setup() {
   // put setup code here.
   palette = [
@@ -43,53 +34,51 @@ function setup() {
     [90, 83, 81],
     [220, 188, 108]
   ];
+  trivia = "";
+  correctAnswer = "";
+  incorrectAnswer = ["", "", ""];
   createCanvas(600, 600);
   strokeWeight(7);
+  ball = matter.makeBall(width / 2, 40, 80);
   floor = matter.makeBarrier(width / 2, height, width, 50);
-  getRandomPalette();
-
-  cannon = {
-    angle: 0,
-    dimensions : {x: width/2, y: height/2, height: 120, width: 1},
-    show : function(){
-      push();
-      translate(this.dimensions.x, this.dimensions.y);
-      rotate(this.angle);
-      rect(0, 0, this.dimensions.width, this.dimensions.height);
-      pop();
-      if (this.hasFired) this.ball.show();
-    },
-    hasFired : false,
-    update : function(){
-      this.angle += 0.01;
-      if (keyIsPressed && !this.hasFired){
-        this.fire();
-        this.hasFired = true;
-      }
-    },
-    fire : function (){
-      this.ball = matter.makeBall(width/2, height/2, 80);
-      this.ball.body.force = {x: 1 - acos(this.angle), y: asin(this.angle)}
-    },
-    ball : null
+  for (var i = 0; i < 3; i++) {
+    barriers.push(matter.makeBarrier((width / 4) * (i + 1), height, 50, 100));
   }
-
-
+  matter.mouseInteraction(canvas);
+  getRandomPalette();
+  getRandomTriviaQuestion(9, DIFFICULTY.EASY, TYPE.MULTIPLE);
+  correctIndex = Math.floor(random(0, 4));
 }
 
 function draw() {
-
   // put the drawing code here
   background(palette[0]);
-
   stroke(palette[3]);
   fill(palette[1]);
-  //floor.show();
+  floor.show();
+  textAlign(CENTER);
+  textSize(32);
+  text(trivia, 0, height / 16, width, height);
+  for (var i = 0; i < barriers.length; i++) {
+    barriers[i].show();
+  }
+  let j = 0;
+  for (var i = 0; i < 4; i++) {
+    textSize(16);
+    textAlign(CENTER);
+    if (i === correctIndex) {
+      text(correctAnswer, ((width / 4) * i) - 20, height - height / 8, 200, 200);
+    } else {
+      text(incorrectAnswer[j], ((width / 4) * i) - 20, height - height / 8, 200, 200);
+      j++;
+    }
+  }
   fill(palette[2]);
-  //ball.show();
-  cannon.update();
-  cannon.show();
+  ball.show();
+  strokeWeight(0);
 }
+
+function initializePalette() {}
 
 function getRandomPalette() {
   var url = "http://colormind.io/api/";
